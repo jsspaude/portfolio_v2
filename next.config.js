9ce7b6db7c7a4b845e4bb42456bc7a77110e2719
Jess Spaude <jess@colonydigital.ca>
2022-10-25 21:27:17 -0700

    nextconfig

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-
+  trailingSlash: true,
   webpack5: true,
   webpack: (config) => {
     config.resolve.fallback = { fs: false };
